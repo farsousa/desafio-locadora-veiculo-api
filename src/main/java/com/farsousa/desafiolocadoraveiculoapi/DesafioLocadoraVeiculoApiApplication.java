4ef0df9ce92ff117edd7fd00d6529b983a553a43
farsousa <andersoncaj26@gmail.com>
2023-10-18 21:18:00 -0300

    first commit

---
+++ src/main/java/com/farsousa/desafiolocadoraveiculoapi/DesafioLocadoraVeiculoApiApplication.java
@@ -0,0 +1,13 @@
+package com.farsousa.desafiolocadoraveiculoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLocadoraVeiculoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLocadoraVeiculoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farsousa/desafiolocadoraveiculoapi/DesafioLocadoraVeiculoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farsousa.desafiolocadoraveiculoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLocadoraVeiculoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
